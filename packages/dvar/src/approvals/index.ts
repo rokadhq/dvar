8971aceb70e84ed43ae00e12ca21852779e845d4
Ar <69129049+aryabhthakur@users.noreply.github.com>
2026-06-28 04:50:35 +0530

    feat: integrate approval resume into runtime

---
+++ packages/dvar/src/approvals/index.ts
@@ -0,0 +1,25 @@
+export {
+  createApprovalRequest,
+  approvalBindingPaths,
+  approvalBindings,
+  approvalActionHash,
+  DvarApprovalRequestError,
+  type CreateApprovalRequestOptions
+} from "./request.js";
+export {
+  createHmacApprovalSigner,
+  verifyAndConsumeApprovalGrant,
+  DvarApprovalGrantError,
+  type HmacApprovalSignerOptions,
+  type VerifyApprovalGrantOptions
+} from "./grant.js";
+export { InMemoryApprovalUseStore } from "./store.js";
+export {
+  processApprovalDecision,
+  submitApprovalRequest,
+  type DvarApprovalProcessingOptions
+} from "./runtime.js";
+export {
+  DvarApprovalProviderError,
+  DvarApprovalRejectedError
+} from "./errors.js";
